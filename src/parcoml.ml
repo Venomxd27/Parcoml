be24cc6c461d75f041197b14b5d38539ad52248a
Venomxd27 <laherunitant04@proton.me>
2024-02-14 10:55:55 +0530

    First commit

--- src/parcoml.ml
+++ src/parcoml.ml
@@ -16,6 +16,12 @@ type error =
     pos : int
   }
 
+let input_sub (start : int) (len : int) (s : input) = 
+  {
+    text = String.sub (s.text) start len;
+    pos = s.pos + start
+  }
+
 type 'a parser = 
   {
     run : input -> (input * 'a , error) result 
@@ -36,4 +42,53 @@ let map (f : 'a -> 'b) (p : 'a parser) : 'b parser =
 
 
 let bind (f : 'a -> 'b parser) (p : 'a parser) : ('b parser) = 
-  fail {pos = 0 ; desc = "TODO : Parcoml.bind yet to be implemented"}
+  {run = fun input -> 
+      match p.run input with 
+      | Ok (input' , x) -> (f x).run input'
+      | Error error -> Error error
+     }
+
+let prefix (prefix_str : string) : string parser = 
+  {
+    run = fun input ->
+      let invalid_prefix_argument =  Error  {
+        pos = input.pos;
+        desc = Printf.sprintf "EXpected '%s'" prefix_str 
+   } in
+ 
+      try 
+      let prefix_size = String.length prefix_str in
+      let input_size = String.length input.text in 
+      let prefix = input |> input_sub 0 prefix_size in
+      if prefix.text = prefix_str then 
+        let rest = input |> input_sub prefix_size (input_size-prefix_size) in 
+        Ok (rest , prefix_str) 
+      else 
+          invalid_prefix_argument 
+      with
+      Invalid_argument _ ->
+      invalid_prefix_argument 
+  }
+
+let ( *> ) (p1 : 'a parser) (p2 : 'b parser) : 'b parser = 
+  {
+    run = fun input ->
+      input
+      |> p1.run
+      |>Result.map (fun (input', _) -> p2.run input')
+      |>Result.join 
+  }
+
+let ( <* ) (p1 : 'a parser) (p2 : 'b parser) : 'a parser = 
+  {
+    run = fun input -> 
+              input
+                  |> p2.run 
+                  |> Result.map (fun (input' , x)->
+                                     input'
+                                      |> p2.run
+                                      |> Result.map (fun (input , _ ) -> (input,x))) 
+                  |>Result.join
+  
+  }
+
